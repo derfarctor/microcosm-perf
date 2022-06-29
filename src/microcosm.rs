5fe94b7c021de62ceed857e03d47bf355378ee40
derfarctor <97409490+derfarctor@users.noreply.github.com>
2022-06-29 17:11:37 +0100

    Tweak word checker

--- src/microcosm.rs
+++ src/microcosm.rs
@@ -25,29 +25,6 @@ fn totals_from_lines(lines_and_key: [&[u16]; 14]) -> [u16; 20] {
     }
     totals
 }
-/*
-pub fn message_from_lines_and_key(lines_and_key: [&[u16]; 14], words: &WordList) -> bool {
-    let totals = totals_from_lines(lines_and_key);
-    let mut message = String::with_capacity(20);
-    for i in 0..20 {
-        if PRECALC_LETTERS[totals[i] as usize] == " " || i == 19 {
-            if !words.contains_key(&message) {
-                return false;
-            }
-            message = String::with_capacity(20);
-        } else {
-            message.push_str(PRECALC_LETTERS[totals[i] as usize]);
-        }
-    }
-    // At this stage the message was fully english words, so generate it again and print the information
-    message = String::with_capacity(20);
-    for i in 0..20 {
-        message.push_str(PRECALC_LETTERS[totals[i] as usize]);
-    }
-    println!("\nAll words were english: {}", message);
-    true
-}
-*/
 
 pub fn message_from_lines_and_key(lines_and_key: [&[u16]; 14], words: &WordList) -> bool {
     let totals = totals_from_lines(lines_and_key);
@@ -55,12 +32,15 @@ pub fn message_from_lines_and_key(lines_and_key: [&[u16]; 14], words: &WordList)
     let mut last = 0;
     for i in 0..20 {
         message.push_str(PRECALC_LETTERS[totals[i] as usize]);
-        if PRECALC_LETTERS[totals[i] as usize] == " " || i == 19 {
+        if PRECALC_LETTERS[totals[i] as usize] == " " {
             if !words.contains_key(&message[last..i]) {
                 return false;
             }
             last = i + 1;
         }
+    }
+    if !words.contains_key(&message[last..20]) {
+        return false;
     }
     // At this stage the message was fully english words, so generate it again and print the information
     println!("\n\nFound text, written to found.txt: {}\n", message);
@@ -90,31 +70,4 @@ pub fn message_from_lines_and_key(lines_and_key: [&[u16]; 14], words: &WordList)
     )
     .expect("Failed to write to found.txt");
     true
-}
-
-fn get_readable_lines_and_key(lines_and_key: [&[u16]; 14]) -> String {
-    for i in 0..14 {
-        let matching_idx = 0;
-        let mut page = 0;
-        let matching_page = 0;
-        let is_key = false;
-        for j in 0..16 {
-            let mut is_line_match = true;
-            let is_key_match = true;
-            for k in 0..lines_and_key[i].len() {
-                for l in 0..13 {
-                    if NUMBOOK[l][j][k] != lines_and_key[i][k] {
-                        is_line_match = false;
-                        break;
-                    }
-                    page = l
-                }
-                if is_line_match {
-                    matching_idx = break;
-                }
-                if NUMKEYS[j][k] != lines_and_key[i][k] {}
-            }
-        }
-    }
-    String::from("UNUSED/UNFINISHED FUNCTION")
 }
